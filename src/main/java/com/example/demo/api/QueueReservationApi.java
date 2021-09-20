10f86142216e08ec3d721f7825830993e31977b6
M1C3E4 <64844916+M1C3E4@users.noreply.github.com>
2021-09-20 15:15:21 +0100

    API finished

--- src/main/java/com/example/demo/api/QueueReservationApi.java
+++ src/main/java/com/example/demo/api/QueueReservationApi.java
@@ -1,4 +1,49 @@
 package com.example.demo.api;
 
+import com.example.demo.dao.entity.QueueReservation;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/reservation")
 public class QueueReservationApi {
+
+    private List<QueueReservation> queueReservationList;
+
+    public QueueReservationApi(){
+        queueReservationList = new ArrayList<>();
+        queueReservationList.add(new QueueReservation(1, "Maciej", "Oklinski", LocalDate.of(2021, 12, 11 )));
+        queueReservationList.add(new QueueReservation(2, "Wiktoria", "Markiewicz", LocalDate.of(2021, 11, 5)));
+    }
+
+    @GetMapping("/all")
+    public List<QueueReservation> getAll(){
+        return queueReservationList;
+    }
+
+    @GetMapping
+    public QueueReservation getById(@RequestParam int index){
+         Optional<QueueReservation> first = queueReservationList.stream().filter(element -> element.getId() == index).findFirst();
+        return first.get();
+    }
+
+    @PostMapping
+    public boolean addQueue (@RequestBody QueueReservation queueReservation){
+        return queueReservationList.add(queueReservation);
+    }
+
+    @PutMapping
+    public boolean updateQueue (@RequestBody QueueReservation queueReservation){
+        return queueReservationList.add(queueReservation);
+    }
+
+    @DeleteMapping
+    public boolean deleteQueue(@RequestParam int index){
+        return queueReservationList.removeIf(element -> element.getId() == index);
+    }
+
 }
--- src/main/java/com/example/demo/dao/entity/QueueReservation.java
+++ src/main/java/com/example/demo/dao/entity/QueueReservation.java
@@ -1,4 +1,66 @@
 package com.example.demo.dao.entity;
 
+import java.time.LocalDate;
+
 public class QueueReservation {
+
+    private int id;
+
+    private String name;
+
+    private String surName;
+
+    private LocalDate localDate;
+
+    public QueueReservation() {
+    }
+
+    public QueueReservation(int id, String name, String surName, LocalDate localDate) {
+        this.id = id;
+        this.name = name;
+        this.surName = surName;
+        this.localDate = localDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    @Override
+    public String toString() {
+        return "QueueReservation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", localDate=" + localDate +
+                '}';
+    }
 }
